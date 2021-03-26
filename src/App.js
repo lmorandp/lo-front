f80a34cac7c169ef6e43b2cdddc63477e679c60f
lmorandp <winstonmoton47@gmail.com>
2021-03-26 12:36:14 -0700

    adding browserhistory package to remove '#' from url

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import * as React from "react";
 import { HydraAdmin } from "@api-platform/admin";
 
 import authProvider from "./components/authProvider";
+import { createBrowserHistory } from 'history';
 import MyLayout from './layout/MyLayout';
 import DataProvider from './components/DataProvider.js'
 import Resources from './components/Resources';
 
 const entrypoint = process.env.REACT_APP_API_ENTRYPOINT;
+const history = createBrowserHistory();
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => (
-    <HydraAdmin entrypoint={entrypoint} layout={MyLayout}  dataProvider={DataProvider}  authProvider = {authProvider}
+    <HydraAdmin
+        entrypoint={entrypoint}
+        layout={MyLayout}
+        history={history}
+        dataProvider={DataProvider}
+        authProvider = {authProvider}
     >
         {Resources}
     </HydraAdmin>
